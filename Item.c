3d7cf28ab90d1e7b79393a40926e88154227d217
Flacx <blueplant@medionmail.com>
2017-08-29 15:12:38 +0200

    Expansion for other items/Added freeing functions

--- Item.c
+++ Item.c
@@ -34,6 +34,32 @@ typedef struct Weapon {
     float atk;
 } Weapon;
 
+typedef struct Book {
+    ItemCore *itemcore;
+    char *content;
+} Book;
+
+typedef struct MiscItem {
+    ItemCore *itemcore;
+    char *description;
+    float x;
+} MiscItem;
+
+Book* makeBook(char *name, float value, enum TYPE type, char *content) {
+    Book *b = NULL;
+    b = (Book*) malloc(sizeof(Book));
+    if (b == NULL) {
+        printf("Allocation of Book failed\n");
+        return NULL;
+    } else {
+        ItemCore *ic = makeItemCore(NULL, value, type);
+        strcpy(ic->name, name);
+        b->itemcore = ic;
+        strcpy(b->content, content);
+        return b;
+    }
+}
+
 Weapon* makeWeapon(char *name, float value, enum TYPE type, float atk) {
     Weapon *w = NULL;
     w = (Weapon*) malloc(sizeof(Weapon));
@@ -41,11 +67,23 @@ Weapon* makeWeapon(char *name, float value, enum TYPE type, float atk) {
         printf("Allocation of Weapon failed\n");
         return NULL;
     } else {
-        ItemCore *ic = makeItemCore(name, value, type);
+        ItemCore *ic = makeItemCore(NULL, value, type);
+        strcpy(ic->name, name);
         w->itemcore = ic;
         w->atk = atk;
         return w;
     }
+}
+
+void deleteItemCore(ItemCore *ic) {
+    free(ic);
+    ic = NULL;
+}
+
+void deleteWeapon(Weapon *w) {
+    deleteItemCore(w->itemcore);
+    free(w);
+    w = NULL;
 }
 
 int main() {
